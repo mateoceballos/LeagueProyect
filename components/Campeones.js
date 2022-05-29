abcf9de35cc7fac1ce4a9ba944795346c5f7622c
Mateo Ceballos <mateo.ceballos@gopass.com.co>
2022-05-28 20:56:06 -0500

    Corrección en campeones

--- components/Campeones.js
+++ components/Campeones.js
@@ -1,7 +1,7 @@
 /* En esta clase recibimos los datos de los campeones, su nombre, rol y niveles disponibles en el juego
   con estos datos le asignamos un ataque especial
 */
-class Lol {
+class LolP {
   #nombre = "";
   #rol = ""; 
   #carril = ""; 
@@ -34,5 +34,34 @@ class Lol {
     return `${nombre}, Ataque base`;
   }
 }
+class Lol {
+  #nombre = "";
+  #rol = ""; 
+  #nivel = [];
+
+  constructor(nombre, rol, nivel) {
+    this.#nombre = nombre;
+    this.#rol = rol;
+    this.#nivel = nivel;
+  }
+
+  get nombre() {
+    return this.#nombre;
+  }
+
+  get rol() {
+    return this.#rol;
+  }
+
+  get nivel() {
+    return this.#nivel;
+  }
+
+  get ataque() {
+    const { nombre } = this;
+    return `${nombre}, Ataque base`;
+  }
+}
 
 export default Lol;
+export {LolP};
